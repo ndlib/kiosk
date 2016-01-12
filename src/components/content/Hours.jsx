602f54856673b5affdc4a1d516abe09c76fddd01
Dan Wolfe <dwolfe2@nd.edu>
2016-01-12 12:43:44 -0500

    hours

--- src/components/content/Hours.jsx
+++ src/components/content/Hours.jsx
@@ -5,12 +5,33 @@ class Default extends Component {
 
     constructor(props, content) {
         super(props, content);
+        this.state = { data: null }
+    }
+
+    loadHours() {
+      $.ajax({
+        context: this,
+        type: 'GET',
+        url: 'https://api.library.nd.edu/1.0/locations/hours.json?auth_token=hours',
+        dataType: 'json',
+        success: function(result){
+          this.setState({data: result});
+        },
+        error: function(request, status, thrownError) {
+          console.log('There was an error.');
+        }
+      });
+    }
+    componentWillMount() {
+      this.loadHours();
     }
 
     render() {
+      let hours = 'formatted hours go here';
       return (
         <div>
-          Hours of Opperation
+          <h1>Hours of Opperation</h1>
+          {hours}
         </div>
       );
     }

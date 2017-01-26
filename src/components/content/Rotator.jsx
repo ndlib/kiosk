5e4b9c50652eeac9fb391febff298f5a5ce67293
Andy Wetherill <awetheri@nd.edu>
2017-01-26 13:05:36 -0500

    Rotator update.
    # 50-character subject line
    #
    # 72-character wrapped longer description. This should answer:
    #
    # * Why was this change necessary?
    # * How does it address the problem?
    # * Are there any side effects?
    #
    # Include a link to the ticket, if any.

--- src/components/content/Rotator.jsx
+++ src/components/content/Rotator.jsx
@@ -19,11 +19,10 @@ var ReactSlickDemo = React.createClass({
 
 
           <img src='/resources/rotator/spotlight-201612-birds-VERT.jpg' />
-          <img src='/resources/rotator/mlk_unity_summit_vertical.png' />
+          <img src='/resources/rotator/ConcourseOpenHouse.jpg' />
           <img src='/resources/rotator/SubjectLibrarians.jpg' />
           <img src='/resources/rotator/DD_vert-EarlyCatholicAmerica.jpg' />
           <img src='/resources/rotator/North.Entrance.jpg' />
-          <img src='/resources/rotator/walkthewalk.monday.vertical.png' />
 
         </Slider>
       </div>

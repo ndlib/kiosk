69201d8881a7a1543f1a9c8acbaeeeda463a9187
Jon Hartzler <jon.hartzler@gmail.comcoke>
2018-09-26 09:47:34 -0400

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ $1 = "--production" ]
+then
+  bucket="wse-web-kiosk-prod-library-nd-edu-230391840102"
+else
+  bucket="wse-web-kiosk-prep-library-nd-edu-333680067100"
+fi
+
+npm install
+npm run build
+
+cd ./public && aws s3 sync . s3://$bucket --delete --acl public-read
